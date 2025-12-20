44f45f0707f694f3f07d4c71ff6e05a97a8e4f4d
get_shwifty247 <daniel.giemsa@outlook.de>
2025-12-20 23:43:02 +0100

    tippo

--- registry.js
+++ registry.js
@@ -17,7 +17,7 @@ async function loadRegistry() {
                     <h3 class="text-xl font-serif text-primary mb-2">${item.name}</h3>
                     <p class="text-gray-600 mb-2 font-bold">${item.price}</p>
                     <p class="text-gray-600 mb-4">${item.description}</p>
-                    <a href="/${item.link}">Beispiel Link aus einem Online Shop</a>
+                    <a href="${item.link}">Beispiel Link aus einem Online Shop</a>
                     
                     <button 
                         class="reserve-btn px-6 py-2 rounded-full text-sm transition-all ${
