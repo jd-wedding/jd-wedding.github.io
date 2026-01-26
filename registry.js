c3297b8e46c71f86e6d3f37bada56607efa04076
get_shwifty247 <daniel.giemsa@outlook.de>
2026-01-26 22:58:48 +0100

    added gift

--- registry.js
+++ registry.js
@@ -12,7 +12,8 @@ async function loadRegistry() {
             2: 'Carbonator-3_matte-black_45_e20b4fa1-30aa-4354-9b73-ff72f585b777 copy.jpg',
             3: 'Standlüfter.jpg',
             4: 'Saugrobo.jpg',
-            5: 'Butterdose_Puro_Produkt.jpg'
+            5: 'Butterdose_Puro_Produkt.jpg',
+            6: 'Brotschneider.jpg'
         };
 
         const cardsHtml = items.map(item => {
